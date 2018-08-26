126165633273cc0f1101b720d16420efad18dbe3
rffffy <raafay.alam@gmail.com>
2018-08-26 02:59:24 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+console.log('Starting app.js');
+
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js');
+
+var res = notes.addNote();
+console.log(res);
+// var user = os.userInfo();
+//
+// fs.appendFile('greetings.txt', `Hello ${user.username}!`, function (err) {
+//     if (err) {
+//         console.log('Unable to write to file.');
+//     }
+// });
